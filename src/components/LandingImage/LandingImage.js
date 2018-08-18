6d9c25b2fe0187f34d682473cc0e54aea1b5dd9f
Adam Nolte <ajnolte12@gmail.com>
2018-08-17 20:27:49 -0500

    Setup Landing

---
+++ src/components/LandingImage/LandingImage.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import './LandingImage.css';
+
+class LandingImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: '0',
+      hours: '0',
+      minutes: '0',
+      seconds: '0',
+    };
+  }
+
+  componentDidMount = () => {
+    this.updateCountdown();
+    setInterval(this.updateCountdown, 1000)
+  }
+
+  updateCountdown = () => {
+    const now = moment();
+    const endDate = moment('06/01/2019').hours(15).minutes(0).seconds(0);
+    const diff = endDate.diff(now, 'seconds')
+    if (diff <= 0) {
+      this.setState({
+        days: '0',
+        hours: '0',
+        minutes: '0',
+        seconds: '0',
+      });
+      return;
+    }
+
+    const dur = moment.duration(diff, 'seconds');
+    this.setState({
+      days: parseInt(dur.asDays()),
+      hours: dur.hours(),
+      minutes: dur.minutes(),
+      seconds: dur.seconds(),
+    });
+  }
+
+  render() {
+    return (
+      <div className={'landing-image'}>
+        <div>
+          <div>Adam &#38; Lindsay</div>
+          <div>June 1, 2019</div>
+          <div className={'landing-countdown'}>
+            <div>{this.state.days} days</div>
+            <div>{this.state.hours} hours</div>
+            <div>{this.state.minutes} minutes</div>
+            <div>{this.state.seconds} seconds</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingImage;
