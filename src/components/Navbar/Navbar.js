0aa3ad888ad260293dcf7685a63cb721755ea206
Adam Nolte <ajnolte12@gmail.com>
2018-08-28 21:54:32 -0500

    Mobile Nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,14 +5,14 @@ import './Navbar.css';
 
 /*eslint-disable jsx-a11y/href-no-hash*/
 
-const NavLinks = ({ className }) => {
+const NavLinks = ({ className, closeNav }) => {
   return (
     <div className={className}>
-      <a href="#">Home</a>
-      <a href="#OurStory">Our Story</a>
-      <a href="#WeddingParty">Wedding Party</a>
-      <a href="#EventDetails">Event Details</a>
-      <a href="#Accomodations">Accomodations</a>
+      <a href="#" onClick={closeNav}>Home</a>
+      <a href="#OurStory" onClick={closeNav}>Our Story</a>
+      <a href="#WeddingParty" onClick={closeNav}>Wedding Party</a>
+      <a href="#EventDetails" onClick={closeNav}>Event Details</a>
+      <a href="#Accomodations" onClick={closeNav}>Accomodations</a>
     </div>
   );
 }
@@ -23,6 +23,17 @@ class Navbar extends Component {
     this.state = {
       navOpen: false,
     };
+    this.navRef = undefined;
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.closeNav);
+    document.addEventListener('mousedown', this.checkClick);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.closeNav);
+    document.removeEventListener('mousedown', this.checkClick);
   }
 
   toggleNav = () => {
@@ -31,18 +42,36 @@ class Navbar extends Component {
     });
   }
 
+  closeNav = () => {
+    if (this.state.navOpen) {
+      this.setState({
+        navOpen: false,
+      });
+    }
+  }
+
+  checkClick = (ev) => {
+    if (this.navRef !== undefined && this.state.navOpen && !this.navRef.contains(ev.target)) {
+      this.closeNav();
+    }
+  }
+
+  setRef = (element) => {
+    this.navRef = element;
+  }
+
   render() {
     return (
-      <div className={'navbar-wrapper'}>
+      <div className={'navbar-wrapper'} ref={this.setRef}>
         <div>
           <div className={'nav-logo'}>#NewlyNolte</div>
-          {<NavLinks className={'nav-links-desktop'} />}
+          {<NavLinks className={'nav-links-desktop'} closeNav={this.closeNav} />}
           <div className={'nav-hamburger'}>
             <FontAwesomeIcon onClick={this.toggleNav} icon={faBars} />
           </div>
         </div>
         {
-          this.state.navOpen && <NavLinks className={'nav-links-mobile'} />
+          this.state.navOpen && <NavLinks className={'nav-links-mobile'} closeNav={this.closeNav}/>
         }
       </div>
     );
