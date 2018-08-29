395e17006640ab12b61bd872aadf11ea3ffa6c31
Adam Nolte <ajnolte12@gmail.com>
2018-08-28 21:46:41 -0500

    Mobile Nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './Navbar.css';
 
 /*eslint-disable jsx-a11y/href-no-hash*/
 
-const Navbar = () => {
+const NavLinks = ({ className }) => {
   return (
-    <div className={'navbar-wrapper'}>
-      <div>
-        <div className={'nav-logo'}>#NewlyNolte</div>
-        <a href="#">Home</a>
-        <a href="#OurStory">Our Story</a>
-        <a href="#WeddingParty">Wedding Party</a>
-        <a href="#EventDetails">Event Details</a>
-        <a href="#Accomodations">Accomodations</a>
-      </div>
+    <div className={className}>
+      <a href="#">Home</a>
+      <a href="#OurStory">Our Story</a>
+      <a href="#WeddingParty">Wedding Party</a>
+      <a href="#EventDetails">Event Details</a>
+      <a href="#Accomodations">Accomodations</a>
     </div>
   );
+}
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navOpen: false,
+    };
+  }
+
+  toggleNav = () => {
+    this.setState({
+      navOpen: !this.state.navOpen,
+    });
+  }
+
+  render() {
+    return (
+      <div className={'navbar-wrapper'}>
+        <div>
+          <div className={'nav-logo'}>#NewlyNolte</div>
+          {<NavLinks className={'nav-links-desktop'} />}
+          <div className={'nav-hamburger'}>
+            <FontAwesomeIcon onClick={this.toggleNav} icon={faBars} />
+          </div>
+        </div>
+        {
+          this.state.navOpen && <NavLinks className={'nav-links-mobile'} />
+        }
+      </div>
+    );
+  }
 }
 
 export default Navbar;
